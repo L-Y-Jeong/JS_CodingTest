e8da863b6cad2961d0f29c14ab6480fa3bc8a612
Yujeong Lee <124586036+L-Y-Jeong@users.noreply.github.com>
2024-02-02 11:56:43 +0900

    Update 27866_문자와문자열.js

--- BOJ/Level5_문자열/27866_문자와문자열.js
+++ BOJ/Level5_문자열/27866_문자와문자열.js
@@ -1,1 +1,8 @@
-test
+var input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+
+var str = input[0].trim();
+var num = parseInt(input[1]);
+var arr = Array.from(str); //문자열 분리 및 배열 변환
+//or arr = [...str]
+
+console.log(arr[num-1]);
