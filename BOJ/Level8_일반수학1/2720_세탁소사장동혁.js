cdefe9c5c80c71f9386671e01c9552e3d449483f
Yujeong Lee <124586036+L-Y-Jeong@users.noreply.github.com>
2024-03-24 10:21:40 +0900

    Create 2720_세탁소사장동혁.js

---
+++ BOJ/Level8_일반수학1/2720_세탁소사장동혁.js
@@ -0,0 +1,21 @@
+// 쿼터 0.25, 다임 0.10, 니켈 0.05, 페니 0.01
+
+input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+
+for (var i = 1; i <= input[0]; i++){
+    var quar = 0;
+    var dime = 0;
+    var nick = 0;
+    var penn = 0;
+    var res = 0;
+    quar = Math.floor(input[i]/25);
+    res = input[i]%25;
+    dime = Math.floor(res / 10);
+    res = res % 10
+    nick = Math.floor((res) / 5);
+    res = res % 5
+    penn = Math.floor((res) / 1);
+
+    console.log(`${quar} ${dime} ${nick} ${penn}`);
+
+}
